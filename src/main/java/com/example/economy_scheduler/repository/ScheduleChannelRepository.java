ba083cc2f7e56269e5b75539300b7f36c4ac247c
홍동수 <sug5806@naver.com>
2021-05-23 15:25:51 +0900

    어노테이션 누락 추가

--- src/main/java/com/example/economy_scheduler/repository/ScheduleChannelRepository.java
+++ src/main/java/com/example/economy_scheduler/repository/ScheduleChannelRepository.java
@@ -3,9 +3,11 @@ package com.example.economy_scheduler.repository;
 import com.example.economy_scheduler.entity.ScheduleChannel;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ScheduleChannelRepository extends JpaRepository<ScheduleChannel, Long> {
 
     @Override
