f4dbfa5e9188d86b9837d3bd1f36ed42a520a4b6
홍동수 <sug5806@naver.com>
2021-05-23 15:41:35 +0900

    누락 어노테이션 추가

--- src/main/java/com/example/economy_scheduler/EconomySchedulerApplication.java
+++ src/main/java/com/example/economy_scheduler/EconomySchedulerApplication.java
@@ -2,8 +2,10 @@ package com.example.economy_scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EconomySchedulerApplication {
 
     public static void main(String[] args) {
